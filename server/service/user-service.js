81af658412a390b82baea77e4271091cc7fd687d
AndreyOhotnikov <ququruq@gmail.com>
2022-05-03 16:28:58 +0300

    controllers2

---
+++ server/service/user-service.js
@@ -0,0 +1,17 @@
+const UserModel = require('../models/user-model')
+const bcrypt = require('bcrypt')
+const mailService = require('./mail-service')
+
+class UserService {
+    async registration (email, password) {
+      const candidate = await UserModel.findOne({email})
+      if (candidate) {
+        throw new Error(`Пользователь с таким email: ${email} уже существует`)
+      }
+      const hashPssword = await bcrypt.hash(password, 4);
+      const activtionLink = uuid.v4()
+      const user = await UserModel.create({email, hashPssword, activtionLink})
+    }
+}
+
+module.exports = new UserService();
