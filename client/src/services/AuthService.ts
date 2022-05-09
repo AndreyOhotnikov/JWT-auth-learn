15c42ee782a1c2778a629633ccdaa87405a1654a
AndreyOhotnikov <ququruq@gmail.com>
2022-05-09 13:21:06 +0300

    component start

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -4,7 +4,15 @@ import { AuthResponse } from "../models/response/AuthResponse";
 
 export default class AuthService {
   static async login (email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
-    return $api.post()
+    return $api.post<AuthResponse>('/login', {email, password})
+  }
+
+  static async registration (email: string, password: string): Promise<AxiosResponse<AuthResponse>> {
+    return $api.post<AuthResponse>('/registration', {email, password})
+  }
+
+  static async logout (): Promise<void> {
+    return $api.post('/logout')
   }
 }
 
